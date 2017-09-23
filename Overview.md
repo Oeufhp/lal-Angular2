a3e25014aecbd8cf4c117d588baefb14d23adf16
Pitarn Harnpanichpun <pitarnh@outlook.com>
2017-09-23 13:37:51 +0700

    initial commit

---
+++ Overview.md
@@ -0,0 +1,9 @@
+Angular 2
+- Module, Component, Services
+- Class, Metadata, Template
+- Directive(*ngIf, *ngFor)
+- Data binding
+- Data display
+- Event listener
+- Transform data
+- User Input
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My awesome app';
+  status = true;
+  people = [
+    {
+      name: 'john',
+      id: 1,
+      url: '../assets/man.svg'
+    },
+    {
+      name: 'jane',
+      id: 2,
+      url: '../assets/woman.svg'
+    }
+  ];
+  appList = ['AAA', 'BBB', 'CCC'];
+  myText = 'Hello world';
+  isClicked = true;
+  name = 'Bob';
+  clicked(event) {
+    this.isClicked = !this.isClicked;
+  }
+  
+  // tslint:disable-next-line:member-ordering
+  dateToday = new Date(2017, 8, 13);
 }
